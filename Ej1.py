da5da9cf3c0ff5077de98a832d5e7ce7705b85f6
Guido Montenegro <mazonchin@hotmail.com>
2022-09-07 23:25:18 -0300

    ej

--- Ej1.py
+++ Ej1.py
@@ -1,5 +1,20 @@
-from turtle import clear
+# Se desea crear un programa que simule el funcionamiento basico de un vehiculo.
+# -Crear una clase "Vehiculo" con los atributos : marca(Str),ruedas ( Int ),color(Str),enMarcha(Booleano, por defecto False)
+# -Crear su constructor
+# -Crear el metodo de instancia arrancar() que permita poner en marcha el vehiculo
+# -crear el metodo de instancia tipoVehiculo() que devuelva "Automovil" si el vehiculo tiene 4 ruedas y "Motocicleta" si posee 2 ruedas.
+# -Crear el metodo de instancia mostrar() que muestre por pantalla todos los 4 atributos del vehiculo.
 
+
+# Colocar abajo la comisión y nro de grupo, ademas de los integrantes del grupo ( Nombre y apellido , legajo ).A la hora de hacer el PR colocar nro de grupo y comisión en el titulo del mismo.
+
+# Comisión : 4 
+# Grupo    : 9
+# Integrantes: Bruno Menna, Guido Montenegro, Nihuel Spinelli, Francisco Urquiza
+# -Legajo 50625, Bruno Menna
+# -Legajo 50622, Guido Montenegro
+# -Legajo 50512, Nihuel Spinelli
+# -Legajo 50505, Francisco Urquiza
 
 class Vehiculo:
     def __init__(self, marca, color, ruedas, enMarcha=False):
