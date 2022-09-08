68abcfd9100b433e7e445f0ff9787fd0f9404034
IsTincho <segninitincho@gmail.com>
2022-09-07 21:53:14 -0300

    Create pre-practica3.py

---
+++ pre-practica3.py
@@ -0,0 +1,16 @@
+class Vehículo():
+    
+   marca = str
+   ruedas = int
+   color = str
+   enMarcha = bool
+   
+def __init__(self, marca,ruedas,color,enMarcha=False):
+    self.marca  = marca
+    self.ruedas = ruedas
+    self.color  = color
+    self.enMarcha   = enMarcha
+
+def arrancar(self,marca,color):
+    self.enMarcha = True
+    print("Arrancando el "+self.marca+" de color: "+self.color)
